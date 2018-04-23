5ea059d761dd673d574e2dff8a3fa8cd6f587d3e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create help navigation page layout using ASPxTreeView and ASPxSplitter
+
+
+<p>This sample  demonstrates a  possible layout basis for help navigation pages using ASPxTreeView and ASPxSplitter.  Also, the demo shows how to expand all the ancestors of the treeview selected node and set the scroll position to it on callback.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,40 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxTreeView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) Then
+			Me.treeView.DataBind()
+			Me.treeView.ExpandToDepth(1)
+		End If
+	End Sub
+
+	Protected Sub callbackPanel_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+		Dim foundNode As TreeViewNode = FindTextInNodesRecursive(Me.tbSearchQuery.Text.ToLower().Trim(), Me.treeView.Nodes)
+		Me.treeView.SelectedNode = foundNode
+		If foundNode IsNot Nothing Then
+			Me.treeView.ExpandToNode(foundNode)
+		End If
+	End Sub
+
+	Protected Function FindTextInNodesRecursive(ByVal text As String, ByVal nodes As TreeViewNodeCollection) As TreeViewNode
+		For Each node As TreeViewNode In nodes
+			If node.Text.ToLower().Contains(text) Then
+				Return node
+			End If
+			If node.Nodes.Count > 0 Then
+				Dim foundNode As TreeViewNode = FindTextInNodesRecursive(text, node.Nodes)
+				If foundNode IsNot Nothing Then
+					Return foundNode
+				End If
+			End If
+		Next node
+		Return Nothing
+	End Function
+End Class
